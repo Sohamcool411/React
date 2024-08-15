7c9efb75d1b234a6a101aff0ed293886d2591548
soham <sohamcool411@gmail.com>
2024-08-15 19:28:23 +0530

    chnage

--- 08_reactRouter/src/main.jsx
+++ 08_reactRouter/src/main.jsx
@@ -10,43 +10,43 @@ import User from './User/User.jsx'
 import  {githubLoader} from './Github/Github.jsx'
 import Github  from './Github/Github.jsx'
 
-// const router =  createBrowserRouter([
-//   { 
-//     path:'/',
-//     element:<Layout/>,
-//     children:[
-//       {
-//         path:"",
-//         element:<Home/>
-//       },
-//       {
-//         path:"about",
-//         element:<About/>,
-//       },
-//       {
-//         path:'contact',
-//         element:<Contact/>
-//       }
-//     ]
-//   }
-// ])
+const router =  createBrowserRouter([
+  { 
+    path:'/',
+    element:<Layout/>,
+    children:[
+      {
+        path:"",
+        element:<Home/>
+      },
+      {
+        path:"about",
+        element:<About/>,
+      },
+      {
+        path:'contact',
+        element:<Contact/>
+      }
+    ]
+  }
+])
 
 
-const router= createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<Layout/>}>
-      <Route path='' element={<Home/>}/>
-      <Route path='about' element={<About/>}/>
-      <Route path='contact' element={<Contact/>}/>
-      <Route path='user/:userId' element={<User/>}/>
-      <Route 
-      loader={githubLoader}
-      path='github' 
-      element={<Github/>}/>
+// const router= createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path='/' element={<Layout/>}>
+//       <Route path='' element={<Home/>}/>
+//       <Route path='about' element={<About/>}/>
+//       <Route path='contact' element={<Contact/>}/>
+//       <Route path='user/:userId' element={<User/>}/>
+//       <Route 
+//       loader={githubLoader}
+//       path='github' 
+//       element={<Github/>}/>
 
-    </Route>
-  )
-)
+//     </Route>
+//   )
+// )
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
